e548ff748847632b7051ad600930d9bf4f14bcd5
Shimmy Weitzhandler <2716316+weitzhandler@users.noreply.github.com>
2020-03-23 12:46:53 +0200

    Add project files.

---
+++ PostgrePlayground/AppDbContext.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using Microsoft.EntityFrameworkCore;
+
+using NpgsqlTypes;
+
+namespace PostgrePlayground
+{
+  public class AppDbContext : DbContext
+  {
+    public DbSet<Contact> Contacts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseNpgsql(@"Host=localhost;Username=postgres;Database=playground;Password=Sf_@dG3fm)+2");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder
+        .Entity<Contact>(contact =>
+        {
+          contact
+          .Property(c => c.Address)
+          .HasColumnType("jsonb");
+
+          contact
+          .Property(c => c.Phones)
+          .HasColumnType("jsonb");
+        });
+    }
+  }
+
+  public class Contact
+  {
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    [Column(TypeName = nameof(NpgsqlDbType.Jsonb))]
+    public Address Address { get; set; }
+
+    HashSet<Phone> _Phones;
+    [Column(TypeName = nameof(NpgsqlDbType.Jsonb))]
+    public HashSet<Phone> Phones
+    {
+      get => _Phones ??= new HashSet<Phone>();
+      set => _Phones = value;
+    }
+  }
+
+  public class Phone
+  {
+    public string Title { get; set; }
+    public string Number { get; set; }
+
+  }
+
+  public class Address
+  {
+    public string AddressI { get; set; }
+    public string AddressII { get; set; }
+  }
+}
---
+++ PostgrePlayground/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace PostgrePlayground
+{
+  class Program
+  {
+    async static Task Main(string[] args)
+    {
+      var host = Host
+        .CreateDefaultBuilder()
+        .ConfigureServices(services =>
+        {
+          services.AddDbContext<AppDbContext>();
+        })
+        .Build();
+
+      var services = host.Services;
+
+      using (var dbContext = services.GetRequiredService<AppDbContext>())
+      {
+        await dbContext.Database.MigrateAsync();
+
+        dbContext.Contacts.Add(
+          new Contact
+          {
+            Name = "Shimmy",
+            Address = new Address
+            {
+              AddressI = "sdfkjasdf",
+              AddressII = "sdfasdf"
+            },
+            Phones =
+            {
+              new Phone { Title = "Home", Number = "1234567890" },
+              new Phone { Title = "Cell", Number = "0123456789" }
+            }
+          });
+
+        await dbContext.SaveChangesAsync();
+      }
+    }
+  }
+}
